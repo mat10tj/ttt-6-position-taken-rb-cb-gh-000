2c3c590db1e6debc139597bd00dcb22e48313390
Michael Thompson <mat10tj@gmail.com>
2020-02-09 00:19:23 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == "X" || "O"
     true
-  else board[index] == "" || " "
+  else board[index] == "" || board[index] == " "
     false
   end
 end
